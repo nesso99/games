8ef2cb13949ffaffa117bf85aaf8008bb247b147
Sotatek-DucPham <duc.pham@sotatek.com>
2025-04-05 13:48:14 +0700

    chore: puzzle

--- bin/puzzlepieces/src/common/constant.rs
+++ bin/puzzlepieces/src/common/constant.rs
@@ -1,6 +1,4 @@
-use bevy::math::Vec2;
-
 pub const RESOLUTION_WIDTH: f32 = 660.;
 pub const RESOLUTION_HEIGHT: f32 = 660.;
 
-pub const SIZE_IMAGE: Vec2 = Vec2::new(450.0, 450.0);
+pub const SIZE_TILE: f32 = RESOLUTION_HEIGHT / 3.;
--- bin/puzzlepieces/src/main.rs
+++ bin/puzzlepieces/src/main.rs
@@ -1,8 +1,9 @@
 use bevy::prelude::*;
 use puzzlepieces::{
     board::{Board, Cell, EmptyCell},
-    common::{RESOLUTION_HEIGHT, RESOLUTION_WIDTH},
+    common::{RESOLUTION_HEIGHT, RESOLUTION_WIDTH, SIZE_TILE},
 };
+use rand::seq::IndexedRandom;
 
 fn main() {
     App::new()
@@ -16,7 +17,7 @@ fn main() {
             ..default()
         }),))
         .add_systems(Startup, setup)
-        .add_systems(Update, (Board::update).chain())
+        .add_systems(Update, (Board::update,))
         .run();
 }
 
@@ -27,9 +28,21 @@ fn setup(
 ) {
     commands.spawn(Camera2d);
 
-    let texture = asset_server.load("images/1.jpg");
-    let layout = TextureAtlasLayout::from_grid(UVec2::new(150, 150), 3, 3, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    // let texture = asset_server.load("images/1.jpg");
+    // let layout = TextureAtlasLayout::from_grid(UVec2::new(150, 150), 3, 3, None, None);
+    // let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    let texture1 = asset_server.load("images/1.jpg");
+    let layout1 = TextureAtlasLayout::from_grid(UVec2::new(450/3, 450/3), 3, 3, None, None);
+    let texture_atlas_layout1 = texture_atlas_layouts.add(layout1);
+
+    let texture2 = asset_server.load("images/2.jpg");
+    let layout2 = TextureAtlasLayout::from_grid(UVec2::new(248/3, 248/3), 3, 3, None, None);
+    let texture_atlas_layout2 = texture_atlas_layouts.add(layout2);
+
+    let textures = [(texture1, texture_atlas_layout1),
+        (texture2, texture_atlas_layout2)];
+    let (texture, texture_atlas_layout) = textures.choose(&mut rand::rng()).unwrap();
 
     for row in 0..3 {
         for col in 0..3 {
@@ -46,14 +59,16 @@ fn setup(
             }
 
             commands.spawn((
-                Sprite::from_atlas_image(
-                    texture.clone(),
-                    TextureAtlas {
+                Sprite {
+                    image: texture.clone(),
+                    texture_atlas: Some(TextureAtlas{
                         layout: texture_atlas_layout.clone(),
                         index: row * 3 + col,
-                    },
-                ),
-                Transform::from_xyz(-220.0 + col as f32 * 220.0, 220.0 - row as f32 * 220.0, 0.0),
+                    }),
+                    custom_size: Some(Vec2::new(218.0, 218.0)),
+                    ..Default::default()
+                },
+                Transform::from_xyz(-SIZE_TILE + col as f32 * SIZE_TILE, SIZE_TILE - row as f32 * SIZE_TILE, 0.0),
                 Cell,
             ));
         }
