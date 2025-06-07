1e42c1cb500caf6ed67a62d0a6f0c88f3396bc3f
Sotatek-DucPham <duc.pham@sotatek.com>
2025-06-07 09:37:25 +0700

    feat: arrow

--- bin/bunnybadger/src/arrow.rs
+++ bin/bunnybadger/src/arrow.rs
@@ -21,18 +21,17 @@ impl ArrowService {
 
     pub fn spawn(commands: &mut Commands, dude: &Dude, game_assets: &Res<GameAssets>) {
         let movement_direction = dude.rotation * Vec3::X;
-        commands
-            .spawn((
-                Sprite::from_image(game_assets.arrow_texture.clone()),
-                Transform {
-                    translation: dude.coords.extend(0.),
-                    rotation: dude.rotation,
-                    ..default()
-                },
-                Velocity(movement_direction.truncate().normalize() * 500.),
-                Lifetime(3.),
-            ))
-            .insert(Arrow {});
+        commands.spawn((
+            Sprite::from_image(game_assets.arrow_texture.clone()),
+            Transform {
+                translation: dude.coords.extend(0.),
+                rotation: dude.rotation,
+                ..default()
+            },
+            Velocity(movement_direction.truncate().normalize() * 500.),
+            Lifetime(3.),
+            Arrow,
+        ));
     }
 
     pub fn check_for_collisions(
@@ -42,23 +41,19 @@ impl ArrowService {
         audio: Res<Audio>,
         game_assets: Res<GameAssets>,
     ) {
+        // For better performance with many objects, consider spatial partitioning.
         for (arrow_entity, arrow_transform) in &arrow_query {
+            let arrow_aabb = Aabb2d::new(arrow_transform.translation.truncate(), SIZE_ARROW / 2.0);
             for (badguy_entity, badguy_transform) in &badguy_query {
-                let intersects =
-                    Aabb2d::new(arrow_transform.translation.truncate(), SIZE_ARROW / 2.0)
-                        .intersects(&Aabb2d::new(
-                            badguy_transform.translation.truncate(),
-                            SIZE_BADGUY / 2.0,
-                        ));
-
-                if intersects {
-                    if let Ok(mut arrow_entity) = commands.get_entity(arrow_entity) {
-                        arrow_entity.despawn();
+                let badguy_aabb =
+                    Aabb2d::new(badguy_transform.translation.truncate(), SIZE_BADGUY / 2.0);
+                if arrow_aabb.intersects(&badguy_aabb) {
+                    if let Ok(mut arrow_ref) = commands.get_entity(arrow_entity) {
+                        arrow_ref.despawn();
                     }
-                    if let Ok(mut badguy_entity) = commands.get_entity(badguy_entity) {
-                        badguy_entity.despawn();
+                    if let Ok(mut badguy_ref) = commands.get_entity(badguy_entity) {
+                        badguy_ref.despawn();
                     }
-
                     audio.play(game_assets.enemy_sound.clone()).with_volume(0.5);
                 }
             }
@@ -76,10 +71,13 @@ impl ArrowService {
     ) {
         shoot_timer.timer.tick(time.delta());
         if buttons.just_released(MouseButton::Left) && shoot_timer.timer.finished() {
-            let dude = query.single().expect("dude is empty");
-            Self::spawn(&mut commands, dude, &game_assets);
-            audio.play(game_assets.shoot_sound.clone()).with_volume(0.5);
-            shoot_timer.timer.reset();
+            if let Ok(dude) = query.single() {
+                Self::spawn(&mut commands, dude, &game_assets);
+                audio.play(game_assets.shoot_sound.clone()).with_volume(0.5);
+                shoot_timer.timer.reset();
+            } else {
+                warn!("Dude entity missing while trying to shoot arrow");
+            }
         }
     }
 }
